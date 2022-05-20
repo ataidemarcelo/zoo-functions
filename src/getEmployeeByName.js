d073489156eb6fd05ed03501ec5679d00af0a562
Marcelo Ataíde <msa.trybe@gmail.com>
2022-05-20 17:50:37 -0300

    Requisito 3 - Crie uma função que busca as pessoas colaboradoras a partir do primeiro ou último nome

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const result = data.employees.find(
+    (employee) => employee.firstName === employeeName || employee.lastName === employeeName,
+  );
+  return result;
 }
 
 module.exports = getEmployeeByName;
