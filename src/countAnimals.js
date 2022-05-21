8eab8ac38d89b03fbc71a99727c12076118c614d
Marcelo Ataíde <msa.trybe@gmail.com>
2022-05-21 15:30:08 -0300

    Requisito 5 - Crie uma função que realiza a contagem dos animais de cada espécie

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+function getAllAnimalsAndQtyResidents() {
+  const result = species.reduce((acc, cur) => {
+    acc[cur.name] = cur.residents.length;
+    return acc;
+  }, {});
+  return result;
+}
+
+function getQuantityBySpecie(specieName) {
+  const result = species.find((specie) => specie.name === specieName);
+  return result.residents.length;
+}
+
+function getQuantityBySpecieAndSex(specieName, sex) {
+  const { residents } = species.find((specie) => specie.name === specieName);
+  const result = residents.filter((resident) => resident.sex === sex);
+  return result.length;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal && animal.sex) {
+    return getQuantityBySpecieAndSex(animal.specie, animal.sex);
+  }
+  if (animal && animal.specie) {
+    return getQuantityBySpecie(animal.specie);
+  }
+  return getAllAnimalsAndQtyResidents();
 }
 
 module.exports = countAnimals;
