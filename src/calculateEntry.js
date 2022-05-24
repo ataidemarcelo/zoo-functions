5106a415f81c539633b0723bd7e5fe362716ad90
Marcelo Ataíde <msa.trybe@gmail.com>
2022-05-24 10:36:26 -0300

    Requisito 8 - Crie o cálculo total do preço de entrada (calculateEntry)

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui!!!
+  const result = entrants.reduce((acc, cur) => {
+    if (cur.age < 18) {
+      acc.child += 1;
+    }
+    if (cur.age >= 18 && cur.age < 50) {
+      acc.adult += 1;
+    }
+    if (cur.age >= 50) {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
+
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const paramNotExistOrEmpty = !entrants || Object.keys(entrants).length === 0;
+
+  if (paramNotExistOrEmpty) {
+    return 0;
+  }
+
+  const { prices } = data;
+  const { child, adult, senior } = countEntrants(entrants);
+  const valueOfEntries = (prices.child * child) + (prices.adult * adult) + (prices.senior * senior);
+
+  return valueOfEntries;
 }
 
 module.exports = { calculateEntry, countEntrants };
