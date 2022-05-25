f47d2906f63efddae37d5584ca1a56a0fcd2710a
Marcelo Ataíde <msa.trybe@gmail.com>
2022-05-25 09:13:14 -0300

    Requisito 10 - Crie uma função que busca o animal mais velho da primeira espécie gerenciada

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { employees, species } = data;
+
+function getEmployeeById(employeeId) {
+  return employees.find((employee) => employee.id === employeeId);
+}
+
+function getFirstSpecieDataById(firstSpecieId) {
+  return species.find((specie) => specie.id === firstSpecieId);
+}
+
+function getOldestResident(residentsFirstspecieData) {
+  let currentResident = residentsFirstspecieData[0];
+
+  residentsFirstspecieData.forEach((resident) => {
+    const isResidentAgeBiggerThanCurrentAge = resident.age > currentResident.age;
+    if (isResidentAgeBiggerThanCurrentAge) {
+      currentResident = resident;
+    }
+  });
+  const oldestResident = currentResident;
+
+  return oldestResident;
+}
+
+function getOldestFromFirstSpecies(employeeId) {
+  const employee = getEmployeeById(employeeId);
+
+  const firstSpecieId = employee.responsibleFor[0];
+  const firstSpecieData = getFirstSpecieDataById(firstSpecieId);
+  const firstSpecieResidents = firstSpecieData.residents;
+
+  const oldestResident = getOldestResident(firstSpecieResidents);
+  const oldestFromFirstSpecies = Object.values(oldestResident);
+
+  return oldestFromFirstSpecies;
 }
 
 module.exports = getOldestFromFirstSpecies;
