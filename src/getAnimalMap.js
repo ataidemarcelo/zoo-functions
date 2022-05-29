b326d7574210432771975a751c632053cfe0e483
Marcelo Ataíde <msa.trybe@gmail.com>
2022-05-29 13:37:58 -0300

    Refactor - Requisito-14 utilizando o reduce

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,70 +1,49 @@
 const data = require('../data/zoo_data');
 
-function getLocations(speciesCollection) {
-  const locationsUnique = [];
+/**
+ * Requisito 14
+ * Este conteúdo foi refatorado com ajuda das
+ * dicas de como usar o 'reduce', para contruir as chaves e valores de objetos
+ * de forma dinâmica, com conteúdo vindo de um array.
+ * Conteúdo apresentado e explicado por: "Renato Mendes - Turma 22 - Tribo A"
+ * durante sessão de dúvidas na sala de estudos dia 26/05/22
+ * github: https://github.com/natomendes
+ * Exemplo ensinado pelo Renato:
+ * https://dev.to/_bigblind/quick-tip-transform-an-array-into-an-object-using-reduce-2gh6
+ * Dia com ótimo conteúdo compartilhado, valeu Renatão, Renan, Valmir...E a todos presentes na sessão!!!
+ */
 
-  speciesCollection.forEach((species) => {
-    const hasLocationInLocationsUnique = locationsUnique.includes(species.location);
-    if (!hasLocationInLocationsUnique) {
-      locationsUnique.push(species.location);
-    }
-  });
-
-  return locationsUnique;
-}
-
-function getResidentsNameOfASpecies(species, sex) {
+function getResidentsName(residents, sex) {
+  let residentsName = [];
+  residentsName = residents.map((resident) => resident.name);
   if (sex) {
-    const residents = species.residents.filter((resident) => resident.sex === sex);
-    const residentsName = residents.map((resident) => resident.name);
-    return residentsName;
+    residentsName = residents
+      .filter((resident) => resident.sex === sex)
+      .map((resident) => resident.name);
   }
-  const residentsName = species.residents.map((resident) => resident.name);
   return residentsName;
 }
 
-function getSpeciesNameByLocation(location) {
-  const spesiesNameByLocation = [];
-  data.species.forEach((species) => {
-    if (species.location === location) {
-      spesiesNameByLocation.push(species.name);
-    }
-  });
-  return spesiesNameByLocation;
-}
-
-function getSpeciesNameAndResidentsByLocation(location, sorted, sex) {
-  const speciesNameAndResidentsName = [];
-
-  data.species.forEach((species) => {
-    if (species.location === location) {
-      const result = getResidentsNameOfASpecies(species, sex);
-      const residentsName = sorted ? result.sort() : result;
-      speciesNameAndResidentsName.push({ [species.name]: residentsName });
-    }
-  });
-  return speciesNameAndResidentsName;
+function sortedResidentsName(residentsName, sorted) {
+  return (sorted ? residentsName.sort() : residentsName);
 }
 
 function getAnimalMap(options) {
-  const locations = getLocations(data.species);
-  const objAnimalMap = {};
+  const animalMap = data.species.reduce((acc, species) => {
+    const { name, location, residents } = species;
 
-  locations.forEach((location) => {
-    const speciesNameByLocation = getSpeciesNameByLocation(location);
-    if (!options) {
-      objAnimalMap[location] = speciesNameByLocation;
-      return objAnimalMap;
+    if (!options || !options.includeNames) {
+      return { ...acc, [location]: [...(acc[location] || []), name] };
     }
-    const { includeNames, sorted, sex } = options;
-    const spesiesNameAndResidentsByLocation = getSpeciesNameAndResidentsByLocation(
-      location, sorted, sex,
-    );
-    objAnimalMap[location] = includeNames
-      ? spesiesNameAndResidentsByLocation
-      : speciesNameByLocation;
-  });
-  return objAnimalMap;
+
+    const { sorted, sex } = options;
+    const residentsName = getResidentsName(residents, sex);
+    const speciesAndResidentNames = { [name]: sortedResidentsName(residentsName, sorted) };
+
+    return { ...acc, [location]: [...(acc[location] || []), speciesAndResidentNames] };
+  }, {});
+
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
